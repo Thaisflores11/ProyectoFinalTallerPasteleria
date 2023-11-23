95837ba0a1e0c47912cd48ba946477f3cf078a6c
Thais11 <thaisflores110620@gmail.com>
2023-11-23 01:51:53 -0400

    cambios

---
+++ src/componentes/atomos/logosito.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function logosito() {
+  return (
+    <div>
+      <img className='osito'
+        src="../public/osito2.svg" alt="" />
+    </div>
+  )
+}
+
+export default logosito
