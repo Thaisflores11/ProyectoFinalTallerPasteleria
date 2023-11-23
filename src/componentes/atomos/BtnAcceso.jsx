2bb6550bb8cd745c082d4e5f043febd6dc9ce873
Dilanapm <apolacadilan@gmail.com>
2023-11-23 04:39:23 -0400

    cambios en botones

--- src/componentes/atomos/BtnAcceso.jsx
+++ src/componentes/atomos/BtnAcceso.jsx
@@ -1,18 +1,7 @@
-const textboton =[
-    {
-    id: 1,
-    nombre: "acceso",
-    },
-    {
-    id: 2,
-    nombre: "Registrarse",
-    },
 
-];
 export function BtnAcceso(){
-    return textboton.map((item,index)=> {
         return(
-            <button className="btnlog" key={index}>{item.nombre}</button>
-        );
-    })
+            <button className="btnlog" > Acceso </button>
+        )
+    
 }
